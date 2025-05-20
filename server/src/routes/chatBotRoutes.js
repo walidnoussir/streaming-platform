5fa22320ae4206c84456e55c6ee1fe987253948f
walid <noussirw@gmail.com>
2025-05-20 18:19:21 +0100

    last version befor end

--- server/src/routes/chatBotRoutes.js
+++
@@ -1,8 +0,0 @@
-// import { Router } from "express";
-// import { chatWithAi } from "../controllers/chatBotController.js";
-
-// const router = Router();
-
-// router.post("/chat", chatWithAi);
-
-// export default router;
